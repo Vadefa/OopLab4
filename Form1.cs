8898cbda708733818b24c0ffd5267e24d616358e
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2021-12-06 23:36:08 +0500

    comm

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,43 @@ namespace OopLab4
 {
     public partial class Form1 : Form
     {
+
+        public class Obj
+        {
+            public virtual void someMethod()
+            {
+
+            }
+            public virtual void name()
+            {
+
+            }
+
+        }
+
+        public class MyStorage {
+            private Obj[] storage;
+            protected int iter;
+            protected int size;
+            protected int count;
+
+            MyStorage()
+            {
+                iter = 0;
+                count = 0;
+                size = 1;
+                for (int i = 0; i < size; i++)
+                    storage[i] = null;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
